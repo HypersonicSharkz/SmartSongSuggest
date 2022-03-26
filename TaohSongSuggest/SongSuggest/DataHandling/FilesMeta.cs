31d352bb54cab6be6d24a1fed8c3f8f395a82c01
Davi3684 <davidhulstroem@gmail.com>
2022-03-26 09:30:55 +0100

    crash fix

--- TaohSongSuggest/SongSuggest/DataHandling/FilesMeta.cs
+++ TaohSongSuggest/SongSuggest/DataHandling/FilesMeta.cs
@@ -5,7 +5,7 @@ namespace Data
 
     public class FilesMeta
     {
-        public string top10kVersion { get; set; }
+        public string top10kVersion { get; set; } = "0.0";
         public DateTime top10kUpdated { get; set; }
 
         public String GetLargeVersion()
