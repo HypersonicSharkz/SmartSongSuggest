47760f8e120af33abaf4daf4ae8928e4ea685190
Davi3684 <davidhulstroem@gmail.com>
2022-04-10 17:57:18 +0200

    removed old files

--- TaohSongSuggest/SongSuggest_Old/Settings/FilterSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Settings
-{
-    public class FilterSettings
-    {
-        public double modifierPP { get; set; } = 100.0;
-        public double modifierStyle { get; set; } = 100.0;
-        public double modifierOverweight { get; set; } = 50.0;
-    }
-}
